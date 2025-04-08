204c0f78c1f13b62bd0952d98c3e163adf87d649
Mousa-Saresi <mousa.saresi@gmail.com>
2025-04-07 23:51:38 -0700

    ASP.NET

---
+++ Data/CoffeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Coffee_Shop2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee_Shop2.Data
+{
+    public class CoffeeDbContext : DbContext
+    {
+        internal object Coffee;
+        internal object CoffeeShop;
+
+        public CoffeeDbContext(DbContextOptions<CoffeeDbContext> options) :base(options) 
+        {
+            
+        }
+        public DbSet<Coffee> CoffeeShope { get; set; }
+    }
+
+  
+}
---
+++ Migrations/CoffeeDb/20250215013059_AddCoffeeToDb.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coffee_Shop2.Migrations.CoffeeDb
+{
+    /// <inheritdoc />
+    public partial class AddCoffeeToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CoffeeShope",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<double>(type: "float", nullable: false),
+                    Time = table.Column<double>(type: "float", nullable: false),
+                    Person = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CoffeeShope", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CoffeeShope");
+        }
+    }
+}
